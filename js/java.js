693e1a9656f4925fafd809a69a4e30b33b882a5b
plalgiush <ilyasushko@yandex.ru>
2020-08-19 13:55:55 +0300

    first commit

---
+++ js/java.js
@@ -0,0 +1,20 @@
+// console.log(1);
+"use strict"; // работа в современном режиме
+
+let number = 5;
+let Number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+alert(5);
+[].push('a');
---
+++ js/интерполяция.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);  // используется только с косыми кавычками
+
+const user = 'Ivan';
+alert(`Привет, ${user}`);
+
