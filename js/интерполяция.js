1e6253d60fe15fe97568b8ace18635ced9d5225e
plalgiush <ilyasushko@yandex.ru>
2020-09-09 21:09:19 +0300

    jQueryAPP

--- js/интерполяция.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const category = 'toys';
-
-console.log(`https://someurl.com/${category}/5`);  // используется только с косыми кавычками
-
-const user = 'Ivan';
-alert(`Привет, ${user}`);
-
