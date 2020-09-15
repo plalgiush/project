90903a2399176b7f8992a782d6b9c164fb946bf4
plalgiush <ilyasushko@yandex.ru>
2020-09-15 16:08:28 +0300

    the next lvl

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
-"use strict";
+
+	var elem = document.body.querySelector('.btn');  //ссылка на кнопку
+
+	elem.onclick = function () {  // что будет происходить после нажатия на кнопку
+
+		var td = document.body.querySelector('.tdd');
+
+		tdd.randColor[rand].background = "red"
+
+		var randColor = ['#FF0000', '#ff1493', '#ffa500', '#FFFF00',
+					 	 '#800080', '#DEB887', '#000000', '#C0C0C0',
+					 	 '#FFFFFF', '#00FF00', '#008000', '#00FFFF', '#0000FF']; //массив до обработчика событий
+
+		var rand = Math.floor(Math.random() * randColor.length);
+
+		console.log(randColor[rand]);
+
+		alert(randColor[rand]);
 
 
-var randColor = ['#FF0000', '#FF1493', '#FFA500', '#FFFF00', '#800080', '#DEB887', '#000000', '#C0C0C0', '#FFFFFF', '#00FF00', '#008000', '#00FFFF', '#0000FF'];
+	};
 
-var rand = Math.floor(Math.random() * randColor.length);
 
-console.log(randColor[rand]);
 
-alert(randColor[rand]);
+// $(function () {
+
+// 	$('.btn').click(function () {
+// 		$('.btn').css('background', 'red')
+// 	});
+// });
