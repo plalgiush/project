42eeb9ef712e0a10a0b7c52535e0e01922ba0893
plalgiush <ilyasushko@yandex.ru>
2020-09-16 12:33:42 +0300

    plus buttons

--- js/script.js
+++ js/script.js
@@ -3,20 +3,20 @@
 
 	elem.onclick = function () {  // что будет происходить после нажатия на кнопку
 
-		var td = document.body.querySelector('.tdd');
-
-		tdd.randColor[rand].background = "red"
+		var td1 = document.body.querySelector('.td1'); // ссылка на таблицу
+	
 
 		var randColor = ['#FF0000', '#ff1493', '#ffa500', '#FFFF00',
 					 	 '#800080', '#DEB887', '#000000', '#C0C0C0',
-					 	 '#FFFFFF', '#00FF00', '#008000', '#00FFFF', '#0000FF']; //массив до обработчика событий
+					 	 '#FFFFFF', '#00FF00', '#008000', '#00FFFF', '#0000FF']; 
 
-		var rand = Math.floor(Math.random() * randColor.length);
+		var	rand = Math.floor(Math.random() * randColor.length);
+
+		td1.style.backgroundColor = rand; 
 
 		console.log(randColor[rand]);
 
 		alert(randColor[rand]);
-
 
 	};
 
@@ -25,6 +25,6 @@
 // $(function () {
 
 // 	$('.btn').click(function () {
-// 		$('.btn').css('background', 'red')
+// 		$('.td1').css('background', 'red')
 // 	});
 // });
